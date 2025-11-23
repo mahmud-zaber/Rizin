50d3d47b9ef96825d7f9552ecb96b81aca446ce2
mahmud-zaber <79710794+mahmud-zaber@users.noreply.github.com>
2025-11-24 01:31:39 +0600

    Update save.php

--- save.php
+++ save.php
@@ -1,13 +1,1 @@
-<?php
-$data = json_decode(file_get_contents("php://input"), true);
 
-if (!empty($data["data"])) {
-    $text = implode("\n\n", $data["data"]);
-    $filename = "rizin_memory_maze_" . date("Y-m-d_H-i-s") . ".txt";
-
-    file_put_contents($filename, $text);
-    echo "saved";
-} else {
-    echo "error";
-}
-?>
